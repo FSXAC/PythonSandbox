df3b3454a74a85d7beabb34aa83ff5930d1b790c
Muchen He <mansurhe@outlook.com>
2016-07-29 16:59:14 -0700

    New project for Neural Network
    Based off of Stephen Welch projects

---
+++ Machine Learning/Neural Network/Neural Network v0.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 29 16:26:45 2016
+
+NEURAL NETWORK TESTING V0
+
+@author: Muchen
+"""
+
+#Imports
+import numpy as np
+
+class Neural_Network(object):
+    def __init__(self, inputSize, hiddenSize, outputSize):
+        #Define hyper-parameters
+        self.inputLayerSize = inputSize;
+        self.outputLayerSize = outputSize;
+        self.hiddenLayerSize = hiddenSize;
+    
+    def forward(self, X):
+        """Forward Propagation where X is a (m x n) matrix and m is
+        the number of training examples and n is the number of units
+        """
+        #Propagation through the neural network
+
+    def sigmoid(self, z):
+        #Apply sigmoid activation function
+        return 1/(1+np.exp(-z));
+
+def main():
+    #Main program
+    
+    #Get X matrix and y vector
+    XData = [[69, 72, 82],
+         [81, 88, 80],
+         [94, 90, 84]]
+        
+    yData = [63, 74, 83]
+    
+    X = np.array(XData, dtype = float)
+    y = np.array(yData, dtype = float)
+    
+    #Normalize the inputs and outputs
+    
+    
+    NN = Neural_Network(3, 4, 1)
+    for i in range(-10, 10):
+        print(NN.sigmoid(i))
+        
+main()
--- Project - Turtle Graphing Calculator.py
+++ Project - Turtle Graphing Calculator.py
@@ -2,140 +2,129 @@ import math
 import turtle
 
 class Graph():
-	def __init__(self):
-		""" Constructor """
-		
-		# Class variables
-		self.screen = turtle.Screen()
-		self.drawerTurtle, self.gridTurtle, self.axisTurtle = turtle.Turtle(visible = False), turtle.Turtle(visible = False), turtle.Turtle(visible = False)
-		
-		# Class variable modifications
-		self.screen.bgcolor("black")
-		
-		# lift pen for all turtles
-		self.drawerTurtle.penup()
-		self.gridTurtle.penup()
-		self.axisTurtle.penup()
-		
-		self.drawerTurtle.pencolor("red")
-		self.gridTurtle.pencolor("grey")
-		self.axisTurtle.pencolor("white")
-		
-		self.drawerTurtle.speed(100)
-		self.gridTurtle.speed(100)
-		self.axisTurtle.speed(100)
-		
-		# calls some other functions
-		self.setupGrid()
-		self.setupAxis()
-		
-		print("constructor finished")
+    def __init__(self):
+        # Class variables
+        self.screen = turtle.Screen()
+        self.drawerTurtle, self.gridTurtle, self.axisTurtle = turtle.Turtle(visible = False), turtle.Turtle(visible = False), turtle.Turtle(visible = False)
 
-	def setupAxis(self):
-		"""Set up the white axis using axis turtles"""
-		
-		# X AXIS
-		# setup initial positions
-		self.axisTurtle.goto(-500,0)
-		self.axisTurtle.pendown()
+        # Class variable modifications
+        self.screen.bgcolor("black")
 
-		# draw across the screen
-		self.axisTurtle.goto(500,0)
-		self.axisTurtle.penup()
-		
-		# Y AXIS
-		# setup initial positions
-		self.axisTurtle.goto(0, -500)
-		self.axisTurtle.pendown()
-		
-		# draw across the screen
-		self.axisTurtle.goto(0, 500)
-		self.axisTurtle.penup()
-		
-		print("axis setup completed")
-		return True
-		
-	def setupGrid(self):
-		"""Set up the grey grid using grid turtles"""
-		
-		step = 100
-		
-		# Create grid
-		for i in range(-500, 500, step):
-			print(i)
-			self.gridTurtle.goto(-500, i)
-			self.gridTurtle.pendown()
-			self.gridTurtle.goto(500, i)
-			
-			self.gridTurtle.penup()
-			
-			self.gridTurtle.goto(i, -500)
-			self.gridTurtle.pendown()
-			self.gridTurtle.goto(i, 500)
-			
-			self.gridTurtle.penup()
+        # lift pen for all turtles
+        self.drawerTurtle.penup()
+        self.gridTurtle.penup()
+        self.axisTurtle.penup()
 
-		print("grid setup completed")
-		return True
-		
-	def draw(self, equation = "x"):
-		""" Draws the function """
-		
-		for x in range(-500, 500, 1):
-			
-			# TRY in case of invalid calculations
-			try:
-				y = eval(equation)
-			except:
-				print("y at x=", x, "does not exist")
-			
-			# move the turtle and draw the trail
-			self.drawerTurtle.goto(x, y)
-			self.drawerTurtle.pendown()
-			
-			print("x=", x, "\ty=", y)
-			
-		self.drawerTurtle.penup()
-		
-		print("function drawn")		
-		return True
-		
-	def clearScreen(self):
-		screen.clear()
-		return True
+        self.drawerTurtle.pencolor("red")
+        self.gridTurtle.pencolor("grey")
+        self.axisTurtle.pencolor("white")
 
-class Main():		
-	def __init__(self):
-		"""the main program loop"""
-		# object and var setup
-		self.grapher = Graph()
-		
-		# The program will always ask for a function / equation
-		while True:
-			userInput = input("EQUATION: y=")
-			print("y=" + userInput)
-			
-			# analyse user input
-			
-			# see if it's valid
-			equationIsValid = self.validateEquation(userInput)
-			
-			# if it is, do calculations and plot the graph
-			if equationIsValid: self.plotEquation(userInput)
-			else: print("SYNTAX ERROR")
-			
-			
-			
-	def validateEquation(self, equation):
-		"""This will take entered equation and see if it is correct or not"""
-		return True
-		
-	def breakEquation(self, equation):
-		"""This will break the equation into smaller parts that Python IDE can then do math on"""
-		return True
+        self.drawerTurtle.speed(100)
+        self.gridTurtle.speed(100)
+        self.axisTurtle.speed(100)
 
-	def plotEquation(self, equation):
-		"""This will take the valid equation and plot it (y vs x)"""
-		self.grapher.draw(equation)
-	
+        # calls some other functions
+        self.setupGrid()
+        self.setupAxis()
+
+        print("constructor finished")
+
+    def setupAxis(self):
+        # X AXIS
+        # setup initial positions
+        self.axisTurtle.goto(-500,0)
+        self.axisTurtle.pendown()
+
+        # draw across the screen
+        self.axisTurtle.goto(500,0)
+        self.axisTurtle.penup()
+
+        # Y AXIS
+        # setup initial positions
+        self.axisTurtle.goto(0, -500)
+        self.axisTurtle.pendown()
+
+        # draw across the screen
+        self.axisTurtle.goto(0, 500)
+        self.axisTurtle.penup()
+
+        print("axis setup completed")
+        return True
+
+    def setupGrid(self):
+        step = 100
+
+        # Create grid
+        for i in range(-500, 500, step):
+            print(i)
+            self.gridTurtle.goto(-500, i)
+            self.gridTurtle.pendown()
+            self.gridTurtle.goto(500, i)
+
+            self.gridTurtle.penup()
+
+            self.gridTurtle.goto(i, -500)
+            self.gridTurtle.pendown()
+            self.gridTurtle.goto(i, 500)
+
+            self.gridTurtle.penup()
+
+        print("grid setup completed")
+        return True
+
+    def draw(self, equation = "x"):
+        for x in range(-500, 500, 1):
+            # TRY in case of invalid calculations
+            try:
+                y = eval(equation)
+            except:
+                ""
+                #print("y at x=", x, "does not exist")
+
+            # move the turtle and draw the trail
+            self.drawerTurtle.goto(x, y)
+            self.drawerTurtle.pendown()
+
+            #print("x=", x, "\ty=", y)
+
+        self.drawerTurtle.penup()
+
+        print("function drawn")
+        return True
+
+    def clearScreen(self):
+        self.screen.clear()
+        return True
+
+class Main():
+    def __init__(self):
+        # object and var setup
+        self.grapher = Graph()
+
+        # The program will always ask for a function / equation
+        while True:
+            userInput = input("EQUATION: y=")
+            print("y=" + userInput)
+
+            # analyse user input
+
+            # see if it's valid
+            equationIsValid = self.validateEquation(userInput)
+
+            # if it is, do calculations and plot the graph
+            if equationIsValid: self.plotEquation(userInput)
+            else: print("SYNTAX ERROR")
+
+    def validateEquation(self, equation):
+        """This will take entered equation and see if it is correct or not"""
+        return True
+
+    def breakEquation(self, equation):
+        """This will break the equation into smaller parts that Python IDE can then do math on"""
+        return True
+
+    def plotEquation(self, equation):
+        """This will take the valid equation and plot it (y vs x)"""
+        self.grapher.draw(equation)
+
 Main()
