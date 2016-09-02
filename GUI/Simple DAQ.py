864a9d5a43be62fcecbf2194a5912c3cd026e53e
Muchen He <mansurhe@outlook.com>
2016-09-01 20:38:31 -0700

    added some gui stuff

--- GUI/Simple DAQ.py
+++ GUI/Simple DAQ.py
@@ -9,8 +9,51 @@ import tkinter as tk
 
 root = tk.Tk()
 
-# top display fraame
-displayFrame = tk.Frame(root)
-displayFrame.pack(fill = "x")
+# top buttons
+topFrame = tk.Frame(root)
+button_select = tk.Button(topFrame, text = "Select HW Module")
+button_quit = tk.Button(topFrame, text = "Quit", command = root.destroy)
+
+button_select.pack(side = "left")
+button_quit.pack(side = "left")
+topFrame.pack()
+
+# row buttons
+midFrame = tk.Frame(root)
+button_trace = tk.Button(midFrame, text = "Trace Signal")
+button_trace_settings = tk.Button(midFrame, text = "Trace Signal Settings")
+button_settings = tk.Button(midFrame, text = "HW Module Settings")
+
+button_trace.pack(side = "left")
+button_trace_settings.pack(side = "left")
+button_settings.pack(side = "left")
+
+midFrame.pack()
+
+# bottom frame
+btnFrame = tk.Frame(root, relief = "sunken")
+
+# display
+display = tk.Canvas(btnFrame, width = 200, height = 80, bd =1, relief = "sunken")
+display.pack(side = "left")
+
+# input panel
+panel = tk.Frame(btnFrame, relief = "sunken")
+label1 = tk.Label(text = "switch #0")
+label2 = tk.Label(text = "switch #1")
+button_switch1_on = tk.Button(panel, text = "on")
+button_switch1_off = tk.Button(panel, text = "off")
+button_switch2_on = tk.Button(panel, text = "on")
+button_switch2_off = tk.Button(panel, text = "off")
+
+label1.pack()
+button_switch1_on.pack(side = "left")
+button_switch1_off.pack(side = "left")
+label2.pack()
+button_switch2_on.pack(side = "left")
+button_switch2_off.pack(side = "left")
+panel.pack()
+
+btnFrame.pack()
 
 root.mainloop()
